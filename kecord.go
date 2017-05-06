0a8aeb80550137948dc711808cbde392ceef3cac
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-05-06 16:39:25 +0800

    updated simple command line parsing

--- kecord.go
+++ kecord.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+    "os"
     "fmt"
 )
 
+func usage() {
+    fmt.Println(
+`Usage: kecord COMMANDS files...
+
+Commands:
+    init    Create an image
+    pull    Pull an image from Docker Hub
+    rm      Remove an image or container
+    ls      List images
+    ps      List containers
+    run     Create a container
+    exec    Execute a command in a running container
+    logs    View logs from a container
+    commit  Commit a container to an image
+    help    Show usage`)
+}
+
+/* TODO */
+func createImage() {}
+func pullImage() {}
+func remove() {}
+func listImages() {}
+func listContainers() {}
+func createContainer() {}
+func execute() {}
+func catLog() {}
+func commit() {}
+
 func main() {
-    fmt.Printf("eh\n")
+    subcmds := map[string]func() {
+        "help":     usage,
+        "init":     createImage,
+        "pull":     pullImage,
+        "rm":       remove,
+        "ls":       listImages,
+        "ps":       listContainers,
+        "run":      createContainer,
+        "exec":     execute,
+        "logs":     catLog,
+        "commit":   commit,
+    }
+
+    cmd := subcmds[os.Args[1]]
+    if (cmd != nil) {
+        cmd()
+    } else {
+        fmt.Printf("Unrecognized command: %s\n\n", os.Args[1])
+        usage()
+        os.Exit(1)
+    }
 }
